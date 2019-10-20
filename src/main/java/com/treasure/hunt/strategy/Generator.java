a5ffc84f5d0a2b99258761e9fa542f4af38cef02
axel12 <alexander.von.trostorff@gmail.com>
2019-10-20 23:16:51 +0200

    something

--- src/main/java/com/treasure/hunt/strategy/Generator.java
+++ src/main/java/com/treasure/hunt/strategy/Generator.java
@@ -2,8 +2,7 @@ package com.treasure.hunt.strategy;
 
 import com.treasure.hunt.strategy.visualisation.VisualisationGeometryItem;
 import com.treasure.hunt.strategy.visualisation.VisualisationGeometryType;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import io.reactivex.Observable;
 import lombok.Getter;
 
 import java.util.ArrayList;
@@ -14,7 +13,7 @@ public abstract class Generator {
     @Getter
     protected final List<VisualisationGeometryType> availableVisualisationGeometryTypes = new ArrayList<>();
     @Getter
-    protected final ObservableList<VisualisationGeometryItem> visualisationGeometryList = FXCollections.observableArrayList();
+    protected final Observable<VisualisationGeometryItem> visualisationGeometryList = null;
 
     public abstract String getDisplayName();
 }
