7f82811f0775daef7d26cd75d4054288527e9211
Ben Rank <ben.rank@posteo.de>
2019-11-05 11:20:02 +0100

    init StrategyFromPaper, added JTSUtils.givePoint()

--- src/main/java/com/treasure/hunt/strategy/searcher/implementations/StrategyFromPaper.java
+++ src/main/java/com/treasure/hunt/strategy/searcher/implementations/StrategyFromPaper.java
@@ -3,15 +3,31 @@ package com.treasure.hunt.strategy.searcher.implementations;
 import com.treasure.hunt.game.GameHistory;
 import com.treasure.hunt.strategy.Product;
 import com.treasure.hunt.strategy.hint.AngleHint;
+import com.treasure.hunt.strategy.hint.HalfplaneHint;
+import com.treasure.hunt.strategy.hint.HalfplaneHint.Direction;
 import com.treasure.hunt.strategy.searcher.Moves;
 import com.treasure.hunt.strategy.searcher.Searcher;
+import com.treasure.hunt.utils.JTSUtils;
+import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.Point;
 
+import static com.treasure.hunt.strategy.hint.HalfplaneHint.angular2correctHalfPlaneHint;
+
 public class StrategyFromPaper implements Searcher<AngleHint> {
+    int phase; //equals j in the paper. In phase i, the algorithm checks a rectangle with a side length of 2^i
+    Point start;
+    Point location;
+    Point A;
+    Point B;
+    Point C;
+    Point D;
 
     @Override
     public void init(Point startPosition, GameHistory gameHistory) {
-
+        start = startPosition;
+        location = (Point) startPosition.copy();
+        phase = 1;
+        setRectToPhase();
     }
 
     @Override
@@ -21,16 +37,52 @@ public class StrategyFromPaper implements Searcher<AngleHint> {
 
     @Override
     public Moves move() {
-        return null;
+        return incrementPhase();
     }
 
     @Override
     public Moves move(AngleHint hint) {
+        double width = B.getX()-A.getX();
+        double height = A.getY()-D.getY();
+        if(width*height <=4) {
+            return incrementPhase();
+        }
+        //now analyse the hint:
+        HalfplaneHint piHint;
+        piHint = angular2correctHalfPlaneHint(hint);
+
         return null;
     }
 
     @Override
     public Point getLocation() {
+        return location;
+    }
+
+    private Moves incrementPhase(){
+        phase++;
+        Point oldA = A;
+        Point oldB = B;
+        Point oldC = C;
+        Point oldD = D;
+        setRectToPhase();
+        return RectangeScan(oldA, oldB, oldC, oldD);
+    }
+
+    private void setRectToPhase()
+    {
+        double halfDiff = Math.pow(2, phase-1);
+        double startX = start.getX();
+        double startY = start.getY();
+        A = JTSUtils.givePoint(startX-halfDiff, startY+halfDiff);
+        B = JTSUtils.givePoint(startX+halfDiff, startY+halfDiff);
+        C = JTSUtils.givePoint(startX-halfDiff, startY+halfDiff);
+        D = JTSUtils.givePoint(startX-halfDiff, startY-halfDiff);
+    }
+
+
+    private Moves RectangeScan(Point A, Point B, Point C, Point D){
         return null;
     }
 }
+
--- src/main/java/com/treasure/hunt/utils/JTSUtils.java
+++ src/main/java/com/treasure/hunt/utils/JTSUtils.java
@@ -1,5 +1,7 @@
 package com.treasure.hunt.utils;
+import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
+import org.locationtech.jts.geom.Point;
 
 public class JTSUtils {
     static GeometryFactory defaultGeometryFactory;
@@ -7,5 +9,8 @@ public class JTSUtils {
         if(defaultGeometryFactory==null)
             defaultGeometryFactory = new GeometryFactory();
         return defaultGeometryFactory;
+    }
+    public static Point givePoint(double x, double y){
+        return getDefaultGeometryFactory().createPoint(new Coordinate(x,y));
     }
 }
