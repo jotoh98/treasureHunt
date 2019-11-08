fd558369db66f036d7b9a03d5b49580abb3a2873
Ben Rank <ben.rank@posteo.de>
2019-11-08 17:19:28 +0100

    forgot to add movesImplementations.Steps.java

--- src/main/java/com/treasure/hunt/strategy/searcher/movesImplementations/Steps.java
+++ src/main/java/com/treasure/hunt/strategy/searcher/movesImplementations/Steps.java
@@ -1,8 +1,9 @@
-package com.treasure.hunt.strategy.searcher;
+package com.treasure.hunt.strategy.searcher.movesImplementations;
 
+import com.treasure.hunt.strategy.searcher.Moves;
 import org.locationtech.jts.geom.Point;
 
-public class Steps extends Moves{
+public class Steps extends Moves {
     public void addStep(Point step){
         addWayPoint(step);
     }
