d6e43f8d9e410b653b6edb87498bfcc01729c282
Ben Rank <ben.rank@posteo.de>
2019-11-17 21:04:26 +0100

    intersection methods removed, continued to work on the strategy

--- src/main/java/com/treasure/hunt/strategy/searcher/implementations/StrategyFromPaper.java
+++ src/main/java/com/treasure/hunt/strategy/searcher/implementations/StrategyFromPaper.java
@@ -7,8 +7,8 @@ import com.treasure.hunt.strategy.hint.HalfplaneHint;
 import com.treasure.hunt.strategy.hint.HalfplaneHint.Direction;
 import com.treasure.hunt.strategy.searcher.Moves;
 import com.treasure.hunt.strategy.searcher.Searcher;
-import com.treasure.hunt.strategy.searcher.movesImplementations.Steps;
 import com.treasure.hunt.utils.JTSUtils;
+import org.locationtech.jts.geom.LineSegment;
 import org.locationtech.jts.geom.LineString;
 import org.locationtech.jts.geom.Point;
 
@@ -43,68 +43,50 @@ public class StrategyFromPaper implements Searcher<AngleHint> {
 
     @Override
     public Moves move(AngleHint hint) {
-        double width = B.getX()-A.getX();
-        double height = A.getY()-D.getY();
-        if(width*height <=4) {
+        double width = B.getX() - A.getX();
+        double height = A.getY() - D.getY();
+        if (width * height <= 4) {
             return incrementPhase();
         }
         //now analyse the hint:
         HalfplaneHint piHint;
         piHint = angular2correctHalfPlaneHint(hint);
 
-        Point []intersection_AD_hint = JTSUtils.lineLinesegmentIntersection(piHint.getHalfplanePointOne(), piHint.getHalfplanePointTwo(), A, D);
-        Point []intersection_BC_hint = JTSUtils.lineLinesegmentIntersection(piHint.getHalfplanePointOne(), piHint.getHalfplanePointTwo(), B, C);
-        if(intersection_AD_hint.length>1)//TODO maybe use another exception
-            throw new IllegalArgumentException("The hint intersects in more than one point with the Line AD");
-        if(intersection_BC_hint.length>1)
-            throw new IllegalArgumentException("The hint intersects in more than one point with the Line BC");
+        LineSegment AB = new LineSegment(A.getCoordinate(), B.getCoordinate());
+        LineSegment BC = new LineSegment(B.getCoordinate(), C.getCoordinate());
+        LineSegment CD = new LineSegment(C.getCoordinate(), D.getCoordinate());
+        LineSegment AD = new LineSegment(A.getCoordinate(), D.getCoordinate());
 
-        if(piHint.getDirection()==Direction.up){
-            if(intersection_AD_hint.length == 0){
-                throw new IllegalArgumentException("The line doesn't intersect with AD and the direction is up," +
-                        " that is not possible.");
-            }
-            if((intersection_AD_hint[0].getY()-D.getY())>=1){
-                D = intersection_AD_hint[0];
-                C = intersection_BC_hint[0];
-                return stepsToCenterOfRectangle(A,B,C,D);
-            }
+        LineSegment piHintLine = new LineSegment(piHint.getHalfplanePointOne().getCoordinate(),
+                piHint.getHalfplanePointTwo().getCoordinate());
+
+        Point intersection_AD_hint = JTSUtils.lineLinesegmentIntersection(piHintLine, AD);
+        Point intersection_BC_hint = JTSUtils.lineLinesegmentIntersection(piHintLine, BC);
+
+        Point intersection_AB_hint = JTSUtils.lineLinesegmentIntersection(piHintLine, AB);
+        Point intersection_CD_hint = JTSUtils.lineLinesegmentIntersection(piHintLine, CD);
+
+        Point []horizontalSplit = splitsRectangleHorizontally(A,B,C,D, piHint, intersection_AD_hint,
+                intersection_BC_hint);
+        if(horizontalSplit != null)
+        {
+            A = horizontalSplit[0];
+            B = horizontalSplit[1];
+            C = horizontalSplit[2];
+            D = horizontalSplit[3];
+            return movesToCenterOfRectangle(A,B,C,D);
         }
-
-        if(piHint.getDirection()==Direction.down){
-            if(intersection_AD_hint.length == 0){
-                   throw new IllegalArgumentException("The line doesn't intersect with AD and the direction is down," +
-                        " that is not possible.");
-            }
-            if((A.getY()-intersection_AD_hint[0].getY())>=1){
-                A = intersection_AD_hint[0];
-                B = intersection_BC_hint[0];
-                return stepsToCenterOfRectangle(A,B,C,D);
-            }
+        Point []verticalSplit = splitsRectangleVertically(A,B,C,D, piHint, intersection_AB_hint,
+                intersection_CD_hint);
+        if(verticalSplit != null)
+        {
+            A = verticalSplit[0];
+            B = verticalSplit[1];
+            C = verticalSplit[2];
+            D = verticalSplit[3];
+            return movesToCenterOfRectangle(A,B,C,D);
         }
-
-        Point []intersection_AB_hint = JTSUtils.lineLinesegmentIntersection(piHint.getHalfplanePointOne(), piHint.getHalfplanePointTwo(), A, B);
-        Point []intersection_CD_hint = JTSUtils.lineLinesegmentIntersection(piHint.getHalfplanePointOne(), piHint.getHalfplanePointTwo(), C, D);
-
-        Point lowerHintPoint;
-        Point upperHintPoint;
-
-        if(piHint.getHalfplanePointOne().getY()<piHint.getHalfplanePointTwo().getY()){
-            lowerHintPoint = piHint.getHalfplanePointOne();
-            upperHintPoint = piHint.getHalfplanePointTwo();
-        }
-        else{
-            lowerHintPoint = piHint.getHalfplanePointTwo();
-            upperHintPoint = piHint.getHalfplanePointOne();
-        }
-
-        if(piHint.getDirection()==Direction.left){
-
-        }
-
-
-
-        return null;
+        return badHintSubroutine(piHint);
     }
 
     @Override
@@ -112,44 +94,151 @@ public class StrategyFromPaper implements Searcher<AngleHint> {
         return location;
     }
 
-    private Steps badHintSubroutine(HalfplaneHint hint){
+    private Point[] splitsRectangleHorizontally(Point A, Point B, Point C, Point D, HalfplaneHint piHint,
+                                                Point intersection_AD_hint, Point intersection_BC_hint) {
+        if (intersection_AD_hint == null || intersection_AD_hint == null) {
+            return null;
+        }
+
+        if (piHint.getDirection() == Direction.up) {
+            if ((intersection_AD_hint.getY() - D.getY()) >= 1) {
+                Point newD = intersection_AD_hint;
+                Point newC = intersection_BC_hint;
+                return new Point[]{A, B, newC, newD};
+            }
+        }
+
+        if (piHint.getDirection() == Direction.down) {
+            if ((A.getY() - intersection_AD_hint.getY()) >= 1) {
+                Point newA = intersection_AD_hint;
+                Point newB = intersection_BC_hint;
+                return new Point[]{newA, newB, C, D};
+            }
+        }
+
+        Point lowerHintPoint;
+        Point upperHintPoint;
+
+        if (piHint.getHalfplanePointOne().getY() < piHint.getHalfplanePointTwo().getY()) {
+            lowerHintPoint = piHint.getHalfplanePointOne();
+            upperHintPoint = piHint.getHalfplanePointTwo();
+        } else {
+            lowerHintPoint = piHint.getHalfplanePointTwo();
+            upperHintPoint = piHint.getHalfplanePointOne();
+        }
+
+        if ((piHint.getDirection() == Direction.left && lowerHintPoint.getX() < upperHintPoint.getX()) ||
+                (piHint.getDirection() == Direction.right && lowerHintPoint.getX() > upperHintPoint.getX()))
+        //the hint points upwards
+        {
+            if (intersection_AD_hint.distance(D) >= 1 && intersection_BC_hint.distance(C) >= 1) {
+                if (intersection_AD_hint.distance(D) >= intersection_BC_hint.distance(C)) {
+                    Point newD = JTSUtils.createPoint(D.getX(), intersection_BC_hint.getY());
+                    Point newC = intersection_BC_hint;
+                    return new Point[]{A, B, newC, newD};
+                } else {
+                    Point newC = JTSUtils.createPoint(C.getX(), intersection_AD_hint.getY());
+                    Point newD = intersection_AD_hint;
+                    return new Point[]{A, B, newC, newD};
+                }
+            }
+        }
+        if ((piHint.getDirection() == Direction.left && lowerHintPoint.getX() > upperHintPoint.getX()) ||
+                (piHint.getDirection() == Direction.right && lowerHintPoint.getX() < upperHintPoint.getX()))
+        //the hint points downwards
+        {
+            if (intersection_AD_hint.distance(A) >= intersection_BC_hint.distance(B)) {
+                Point newA = JTSUtils.createPoint(A.getX(), intersection_BC_hint.getY());
+                Point newB = intersection_BC_hint;
+                return new Point[]{newA, newB, C, D};
+            } else {
+                Point newB = JTSUtils.createPoint(B.getX(), intersection_AD_hint.getY());
+                Point newA = intersection_AD_hint;
+                return new Point[]{newA, newB, C, D};
+            }
+        }
         return null;
     }
 
-    private Steps twoHintsSubroutine(HalfplaneHint firstHint, HalfplaneHint secondHint){
+    private Point[] splitsRectangleVertically(Point A, Point B, Point C, Point D, HalfplaneHint piHint,
+                                              Point intersection_AB_hint, Point intersection_CD_hint) {
+        if (piHint.getDirection() == Direction.left) {
+            if (intersection_AB_hint != null) {
+                if (intersection_AB_hint.distance(B) >= 1 && intersection_CD_hint.distance(C) >= 1) {
+                    // checks if y is bigger or equal to 0
+                    // determine which intersection-point has to be used to calculate the rectangle-points:
+                    if (intersection_AB_hint.distance(B) >= intersection_CD_hint.distance(C)) {
+                        Point newB = JTSUtils.createPoint(intersection_CD_hint.getX(), B.getY());
+                        Point newC = intersection_CD_hint;
+                        return new Point[]{A, newB, newC, D};
+                    } else {
+                        // equivalent
+                        Point newC = JTSUtils.createPoint(intersection_AB_hint.getX(), C.getY());
+                        Point newB = intersection_AB_hint;
+                        return new Point[]{A, newB, newC, D};
+                    }
+                }
+            }
+        }
+
+        if (piHint.getDirection() == Direction.right) {
+            if (intersection_AB_hint != null) {
+                if (intersection_AB_hint.distance(B) >= 1 && intersection_CD_hint.distance(C) >= 1) {
+                    // checks if y is bigger or equal to 0
+                    // determine which intersection-point has to be used to calculate the rectangle-points:
+                    if (intersection_AB_hint.distance(A) >= intersection_CD_hint.distance(D)) {
+                        Point newA = JTSUtils.createPoint(intersection_CD_hint.getX(), A.getY());
+                        Point newD = intersection_CD_hint;
+                        return new Point[]{newA, B, C, newD};
+                    } else {
+                        // equivalent
+                        Point newD = JTSUtils.createPoint(intersection_AB_hint.getX(), D.getY());
+                        Point newA = intersection_AB_hint;
+                        return new Point[]{newA, B, C, newD};
+                    }
+                }
+            }
+        }
         return null;
     }
 
-    private Steps stepsToCenterOfRectangle(Point P1, Point P2, Point P3, Point P4){
+    private Moves badHintSubroutine(HalfplaneHint hint) {
+        return null;
+    }
+
+    private Moves twoHintsSubroutine(HalfplaneHint firstHint, HalfplaneHint secondHint) {
+        return null;
+    }
+
+    private Moves movesToCenterOfRectangle(Point P1, Point P2, Point P3, Point P4) {
         LineString line13 = JTSUtils.createLineString(P1, P3);
-        Steps ret = new Steps();
-        ret.addStep(line13.getCentroid());
+        Moves ret = new Moves();
+        ret.addWayPoint(line13.getCentroid());
         return ret;
     }
 
-    private Moves incrementPhase(){
+    private Moves incrementPhase() {
         phase++;
         Point oldA = A;
         Point oldB = B;
         Point oldC = C;
         Point oldD = D;
         setRectToPhase();
-        return RectangeScan(oldA, oldB, oldC, oldD); //TODO maybe go to the middle of the new rectangle
+        return rectangleScan(oldA, oldB, oldC, oldD); //TODO maybe go to the middle of the new rectangle
     }
 
-    private void setRectToPhase()
-    {
-        double halfDiff = Math.pow(2, phase-1);
+    private void setRectToPhase() {
+        double halfDiff = Math.pow(2, phase - 1);
         double startX = start.getX();
         double startY = start.getY();
-        A = JTSUtils.createPoint(startX-halfDiff, startY+halfDiff);
-        B = JTSUtils.createPoint(startX+halfDiff, startY+halfDiff);
-        C = JTSUtils.createPoint(startX-halfDiff, startY+halfDiff);
-        D = JTSUtils.createPoint(startX-halfDiff, startY-halfDiff);
+        A = JTSUtils.createPoint(startX - halfDiff, startY + halfDiff);
+        B = JTSUtils.createPoint(startX + halfDiff, startY + halfDiff);
+        C = JTSUtils.createPoint(startX - halfDiff, startY + halfDiff);
+        D = JTSUtils.createPoint(startX - halfDiff, startY - halfDiff);
     }
 
 
-    private Moves RectangeScan(Point A, Point B, Point C, Point D){
+    private Moves rectangleScan(Point A, Point B, Point C, Point D) {
         return null;
     }
 }
--- src/main/java/com/treasure/hunt/utils/JTSUtils.java
+++ src/main/java/com/treasure/hunt/utils/JTSUtils.java
@@ -1,9 +1,6 @@
 package com.treasure.hunt.utils;
 import org.locationtech.jts.algorithm.Angle;
-import org.locationtech.jts.geom.Coordinate;
-import org.locationtech.jts.geom.GeometryFactory;
-import org.locationtech.jts.geom.LineString;
-import org.locationtech.jts.geom.Point;
+import org.locationtech.jts.geom.*;
 
 public class JTSUtils {
     static GeometryFactory defaultGeometryFactory;
@@ -22,82 +19,18 @@ public class JTSUtils {
     }
 
     /**
-     * Tests whether the LineString defined by P1 and P2 intersects with the LineString defined by Q1 and Q2.
-     * If they do, the intersecting Points are returned.
-     * @param P1
-     * @param P2
-     * @param Q1
-     * @param Q2
+     * Tests whether line line intersects with the linesegment linesegment
+     * @param line
+     * @param linesegment
      * @return
      */
-    public static Point[] linesegmentIntersection(Point P1, Point P2, Point Q1, Point Q2){
-        Coordinate[] coord1 = {P1.getCoordinate(), P2.getCoordinate()};
-        LineString line1 = getDefaultGeometryFactory().createLineString(coord1);
-        Coordinate[] coord2 = {Q1.getCoordinate(), Q2.getCoordinate()};
-        LineString line2 = getDefaultGeometryFactory().createLineString(coord2);
-        Coordinate[] intersect_coords = (line1.intersection(line2)).getCoordinates();
-        Point[] ret = new Point[intersect_coords.length];
-        for(int i=0; i<intersect_coords.length; i++){
-            ret[i] = createPoint(intersect_coords[i].getX(),intersect_coords[i].getY());
-        }
-        return ret;
-    }
-
-    /**
-     * Tests whether the line defined by P1 and P2 intersects with the line defined by Q1 and Q2.
-     * If they do, the intersecting points are returned.
-     * @param P1
-     * @param P2
-     * @param Q1
-     * @param Q2
-     * @return
-     */
-    public static Point[] lineIntersection(Point P1, Point P2, Point Q1, Point Q2){
-        // Line P represented as a1x + b1y = c1
-        double a1 = P2.getY() - P1.getY(); //B.y - A.y;
-        double b1 = P1.getX() - P2.getX(); //A.x - B.x;
-        double c1 = a1*P1.getX() + b1*P1.getY(); //a1*(A.x) + b1*(A.y);
-
-        // Line Q represented as a2x + b2y = c2
-        double a2 = Q2.getY()-Q1.getY(); //D.y - C.y;
-        double b2 = Q1.getX() - Q2.getX(); //C.x - D.x;
-        double c2 = a2*Q1.getX() + b2*Q1.getY(); //a2*(C.x)+ b2*(C.y);
-
-        double determinant = a1*b2 - a2*b1;
-
-        if (determinant == 0)
-        {
-            if(a2/c2 == a1/c1){
-                return new Point[]{P1, P2, Q1, Q2}; // the lines are equal
-            }
-            return new Point[0];
-        }
-        double x = (b2*c1 - b1*c2)/determinant;
-        double y = (a1*c2 - a2*c1)/determinant;
-        return new Point[]{createPoint(x, y)};
-    }
-
-    /**
-     * Tests whether the line defined by L1 and L2 intersects with the linesegment defined by S1 and S2
-     * @param L1
-     * @param L2
-     * @param S1
-     * @param S2
-     * @return
-     */
-    public static Point[] lineLinesegmentIntersection(Point L1,Point L2,Point S1,Point S2){
-        if(L1==L2 || S1==S2)
-            throw new IllegalArgumentException("L1==L2 or S1==S2");
-        Point[] intersection = lineIntersection(L1, L2, S1, S2);
-        if(intersection.length>1)
-            return new Point[]{S1,S2}; // S lies on L
-
-        if(intersection.length==0)
+    public static Point lineLinesegmentIntersection(LineSegment line, LineSegment linesegment){
+        Point intersection = getDefaultGeometryFactory().createPoint(line.lineIntersection(linesegment));
+        LineString lineSegString = createLineString(getDefaultGeometryFactory().createPoint(linesegment.p0),
+                getDefaultGeometryFactory().createPoint(linesegment.p1));
+        if(lineSegString.contains(intersection)){
             return intersection;
-
-        if((intersection[0].intersection(createLineString(S1,S2))).isEmpty())
-            return new Point[0];
-
-        return intersection;
+        }
+        return null;
     }
 }
