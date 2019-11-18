09e92d1541f0bdc3a5d93412228f9f1cbf98b10e
dorianreineccius <Dorian.Rec@gmail.com>
2019-11-18 15:47:05 +0100

    Fixed some compilation-errors.

--- src/main/java/com/treasure/hunt/strategy/hider/implementations/RandomAngularHintStrategy.java
+++ src/main/java/com/treasure/hunt/strategy/hider/implementations/RandomAngularHintStrategy.java
@@ -1,64 +1,50 @@
 package com.treasure.hunt.strategy.hider.implementations;
 
 import com.treasure.hunt.game.GameHistory;
-import com.treasure.hunt.strategy.Product;
-import com.treasure.hunt.strategy.geom.GeometryItem;
 import com.treasure.hunt.strategy.hider.Hider;
 import com.treasure.hunt.strategy.hint.AngleHint;
 import com.treasure.hunt.strategy.searcher.Moves;
-import jdk.nashorn.internal.objects.annotations.Getter;
-import jdk.nashorn.internal.objects.annotations.Setter;
 import org.locationtech.jts.algorithm.Angle;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Point;
-import org.locationtech.jts.geom.PrecisionModel;
-import lombok.Getter;
-import lombok.Setter;
 
 public class RandomAngularHintStrategy implements Hider<AngleHint> {
     private GeometryFactory factory;
     private Point treasure;
     private GameHistory history;
 
-
+    @Override
+    public Point getTreasureLocation() {
+        return treasure;
+    }
 
     @Override
-    public void init(Point treasurePosition, GameHistory gameHistory) {
+    public void init(GameHistory gameHistory) {
         factory = new GeometryFactory();
-        treasure = treasurePosition;
+        treasure = factory.createPoint(new Coordinate(0, 0));
         // register Runnable to Gamehistory
         history = gameHistory;
 
     }
 
-    @Override
-    public void commitProduct(Product product) {
-
-    }
     /**
-     *
      * @param moves the moves which, contain the players current position as a Point
      * @return AngleHint a 180 degree angle hint whose orientation is counterclockwise i.e. the treasure is to the left of the vector(angleCenter,anglePointOne)
      */
     @Override
-    public AngleHint move(Moves moves){
+    public AngleHint move(Moves moves) {
         Point player = moves.getEndPoint().getObject();
-        double x = (Math.random()*2) -1;
-        double y = (Math.random()*2) -1;
+        double x = (Math.random() * 2) - 1;
+        double y = (Math.random() * 2) - 1;
 
-        Point firstAnglePoint = factory.createPoint(new Coordinate(player.getX() + x,player.getY() + y));
-        Point secondAnglePoint = factory.createPoint(new Coordinate(player.getX() - x,player.getY() - y));
+        Point firstAnglePoint = factory.createPoint(new Coordinate(player.getX() + x, player.getY() + y));
+        Point secondAnglePoint = factory.createPoint(new Coordinate(player.getX() - x, player.getY() - y));
 
-
-        if(Angle.angleBetweenOriented(firstAnglePoint.getCoordinate(),player.getCoordinate(),treasure.getCoordinate())>0){
-            return new AngleHint(firstAnglePoint,player,secondAnglePoint);
-        }else{
-            return new AngleHint(secondAnglePoint,player,firstAnglePoint);
+        if (Angle.angleBetweenOriented(firstAnglePoint.getCoordinate(), player.getCoordinate(), treasure.getCoordinate()) > 0) {
+            return new AngleHint(firstAnglePoint, player, secondAnglePoint);
+        } else {
+            return new AngleHint(secondAnglePoint, player, firstAnglePoint);
         }
-
-
     }
-
-
 }
--- src/main/java/com/treasure/hunt/utils/JTSUtils.java
+++ src/main/java/com/treasure/hunt/utils/JTSUtils.java
@@ -1,34 +1,38 @@
 package com.treasure.hunt.utils;
-import org.locationtech.jts.algorithm.Angle;
+
 import org.locationtech.jts.geom.*;
 
 public class JTSUtils {
     static GeometryFactory defaultGeometryFactory;
-    public static GeometryFactory getDefaultGeometryFactory(){
-        if(defaultGeometryFactory==null)
+
+    public static GeometryFactory getDefaultGeometryFactory() {
+        if (defaultGeometryFactory == null) {
             defaultGeometryFactory = new GeometryFactory();
+        }
         return defaultGeometryFactory;
     }
-    public static Point createPoint(double x, double y){
-        return getDefaultGeometryFactory().createPoint(new Coordinate(x,y));
+
+    public static Point createPoint(double x, double y) {
+        return getDefaultGeometryFactory().createPoint(new Coordinate(x, y));
     }
 
-    public static LineString createLineString(Point A, Point B){
+    public static LineString createLineString(Point A, Point B) {
         Coordinate[] coords = {A.getCoordinate(), B.getCoordinate()};
         return getDefaultGeometryFactory().createLineString(coords);
     }
 
     /**
      * Tests whether line line intersects with the linesegment linesegment
+     *
      * @param line
      * @param linesegment
      * @return
      */
-    public static Point lineLinesegmentIntersection(LineSegment line, LineSegment linesegment){
+    public static Point lineLinesegmentIntersection(LineSegment line, LineSegment linesegment) {
         Point intersection = getDefaultGeometryFactory().createPoint(line.lineIntersection(linesegment));
         LineString lineSegString = createLineString(getDefaultGeometryFactory().createPoint(linesegment.p0),
                 getDefaultGeometryFactory().createPoint(linesegment.p1));
-        if(lineSegString.contains(intersection)){
+        if (lineSegString.contains(intersection)) {
             return intersection;
         }
         return null;
