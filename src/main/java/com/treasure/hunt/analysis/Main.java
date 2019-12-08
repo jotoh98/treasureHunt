abddfd849c3c87058913722adbe17e27ba728fd0
Shlohmo <daniel.bauer@uni-bonn.de>
2019-12-08 22:31:14 +0100

    Needed for Testing

---
+++ src/main/java/com/treasure/hunt/analysis/Main.java
@@ -0,0 +1,9 @@
+package com.treasure.hunt.analysis;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class Main {
+    public static void main(String[] args) throws InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException {
+        new CreatorOfInstances().test();
+    }
+}
