56048f910578cda13792dbbec747880de4edde29
Shlohmo <daniel.bauer@uni-bonn.de>
2019-12-08 22:33:24 +0100

    Recoding for prettiness and usability

--- src/main/java/com/treasure/hunt/analysis/RunInstancesOverview.java
+++ src/main/java/com/treasure/hunt/analysis/RunInstancesOverview.java
@@ -1,15 +1,21 @@
 package com.treasure.hunt.analysis;
 
+
+
+import lombok.Getter;
 import lombok.experimental.Delegate;
 
 import java.util.*;
 import java.util.concurrent.atomic.AtomicReference;
+import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.function.ToDoubleBiFunction;
+import java.util.function.ToDoubleFunction;
 import java.util.stream.Collectors;
 
-public class RunInstancesOverview {
+public class RunInstancesOverview implements Comparable<RunInstancesOverview>{
     @Delegate
-    private List<RunInstanceData> run=new ArrayList<RunInstanceData>();
+    private List<RunInstanceData> run = new ArrayList<RunInstanceData>();
 
     public RunInstancesOverview(List<RunInstanceData> run) {
 
@@ -19,85 +25,97 @@ public class RunInstancesOverview {
     //TODO: implement methods analysing a series of runInstances such as median of the series or minimal linear factor of the approx. solution
 
     //small helpers to keep it dry as your hoe
-    protected double getAverage(Function<RunInstanceData, Double> method){
-       double sum = this.stream().mapToDouble(method::apply).sum();
-       return sum/this.size();
+    private double getAverage(Function<RunInstanceData, Double> method) {
+        if (this.size() == 0) {
+            return 0;
+        }
+        double sum = this.run.stream().mapToDouble(method::apply).sum();
+        return sum / this.size();
 
     }
 
-    protected double getRunningTimeFactor(Function<Double, Double> method){
-        RunInstanceData worst=this.getWorstCase();
-        return worst.getTraceLength()/method.apply(getWorstCase().getOptSolution());
+    protected double getRunningTimeFactor(Function<Double, Double> method) {
+        RunInstanceData worst = this.getWorstCase();
+        return worst.getRunningTimeFactor(method);
 
     }
 
-    protected double getRunningTimeFactor(Function<Double, Double> method,RunInstanceData instanceData){
-        return instanceData.getTraceLength()/method.apply(getWorstCase().getOptSolution());
+    protected double getRunningTimeFactor(Function<Double, Double> method, RunInstanceData instanceData) {
+        return instanceData.getRunningTimeFactor(method);
 
     }
 
-    protected List getListRunningTimeFactors(Function<Double, Double> method){
-        List<Double> factors=new ArrayList<Double>();
-        this.forEach(instance ->{
-            factors.add(this.getRunningTimeFactor(method,instance));
-        });
-        return factors;
-    }
     //end small helpers
 
-    public double getAveragetraceLength(){
-        return getAverage((RunInstanceData data)-> data.getTraceLength());
+    public double getAverageTraceLength() {
+        return getAverage(RunInstanceData::getTraceLength);
     }
 
-    public double getAverageSolutionQuotient(){
-        return getAverage((RunInstanceData data)-> data.getSolutionQuotient());
-
-    }
-
-    public double getAverageHintRequest(){
-        return getAverage((RunInstanceData data)-> (double) data.getHintRequests());
-    }
-
-    public void sortbyWorstCase(){
-        Collections.sort(this.run,((x, y) -> ((Double)x.getSolutionQuotient()).compareTo(y.getSolutionQuotient()) ));
+    public double getAverageSolutionQuotient() {
+        return getAverage(RunInstanceData::getLinearRunningTimeFactor);
 
     }
 
-    public void sortbyBestCase(){
-        this.sortbyWorstCase();
+    public double getAverageHintRequest() {
+        return getAverage((RunInstanceData data) -> (double) data.getHintRequests());
+    }
+
+    public void sortBy(Comparator<RunInstanceData> method) {
+        this.run.sort(method);
+    }
+
+    public void sortByWorstCase() {
+        sortBy(RunInstanceData::compareByRunningTime);
+
+    }
+
+    public void sortByBestCase() {
+        sortByWorstCase();
         Collections.reverse(this.run);
     }
 
     public RunInstanceData getWorstCase() {
-        final RunInstanceData[] worst = {this.get(0)};
-        if(worst[0] ==null) return null;//handle me
-        this.forEach(instance ->{
-            if (instance.getSolutionQuotient()< worst[0].getSolutionQuotient())
-                worst[0] =instance;
-        });
-        return worst[0];
+
+        return this.run.stream().max(RunInstanceData::compareByRunningTime).orElse(null);
+
     }
 
-    public double getLinearRunningTimeFactor() {
-        return this.getRunningTimeFactor((Double opt)->opt);
+    public RunInstanceData getBestCase() {
+        return this.run.stream().min(RunInstanceData::compareByRunningTime).orElse(null);
     }
 
-    public double getQuadraticRunningTimeFactor(){
-        return this.getRunningTimeFactor((Double opt)->opt*opt);
+    public RunInstancesOverview filterByOptSol(double toleranceValue) {
+        List<RunInstanceData> filtered = this.run.stream()
+                .filter(instanceData -> instanceData.getOptSolution() < toleranceValue)
+                .collect(Collectors.toList());
+        return new RunInstancesOverview(filtered);
     }
 
-    public List getListLinearRunningTimeFactors(){
-        return this.getListRunningTimeFactors((Double opt)->opt);
+    @Override
+    public int compareTo(RunInstancesOverview runInstancesOverview) {
+        return 0;
     }
 
-    public RunInstancesOverview filterbyoptSol(double toleranceValue){
-         List<RunInstanceData> filtered= this.run.stream()
-                 .filter(instanceData -> instanceData.getOptSolution() < toleranceValue)
-                 .collect(Collectors.toList());
-         return new RunInstancesOverview(filtered);
+    //@Override add implements SortableList
+    public void sort() {
+        sortBy(RunInstanceData::compareByRunningTime);
     }
 
+    public int compareBy(Function<RunInstancesOverview, Double> method, RunInstancesOverview overview) {
+        return (int) Math.signum(method.apply(this) - method.apply(overview));
+    }
 
-    //TODO add compare functions, which can compare two or more RunInstancesOverviews with eachother
+    public int compareByAvgTraceLength(RunInstancesOverview overview) {
+        return compareBy(RunInstancesOverview::getAverageTraceLength, overview);
+    }
+
+    public int compareByAvgSolutionQuotient(RunInstancesOverview overview) {
+        return compareBy(RunInstancesOverview::getAverageSolutionQuotient, overview);
+    }
+
+    public int compareByAvgHintRequests(RunInstancesOverview overview) {
+        return compareBy(RunInstancesOverview::getAverageHintRequest, overview);
+    }
+
 
 }
