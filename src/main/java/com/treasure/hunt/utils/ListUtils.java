76a01018ecd4a0954a9363e0568cf97643c5323b
anLeid <39446584+anLeid@users.noreply.github.com>
2020-03-10 19:56:53 +0100

    Feature/#165 export widget (#197)
    * added CSV Export and table string
    
    * Merge branch 'master' of C:\Users\Shlohmo\Documents\treasureHunt with conflicts.
    
    * Revert "Merge branch 'master' of C:\Users\Shlohmo\Documents\treasureHunt with conflicts."
    
    This reverts commit c8497a32f2e2924818764b9c405edc205462f1e1.
    
    Co-authored-by: Jonathan Hassel <jonathan.hassel@icloud.com>

--- src/main/java/com/treasure/hunt/utils/ListUtils.java
+++ src/main/java/com/treasure/hunt/utils/ListUtils.java
@@ -1,5 +1,6 @@
 package com.treasure.hunt.utils;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.BiFunction;
@@ -26,5 +27,18 @@ public class ListUtils {
                 .reduce(Function::andThen)
                 .orElse(Function.identity())
                 .apply(input);
+    }
+
+    public static <T> List<List<T>> transpose(List<List<T>> table) {
+        List<List<T>> ret = new ArrayList<List<T>>();
+        final int N = table.get(0).size();
+        for (int i = 0; i < N; i++) {
+            List<T> col = new ArrayList<T>();
+            for (List<T> row : table) {
+                col.add(row.get(i));
+            }
+            ret.add(col);
+        }
+        return ret;
     }
 }
