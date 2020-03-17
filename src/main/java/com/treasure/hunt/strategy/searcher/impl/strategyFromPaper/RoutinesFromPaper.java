ddf823d795ea278974746a9355f78be59080cf4d
Ben Rank <ben.rank@posteo.de>
2020-03-17 16:52:33 +0100

    Redo some javadoc in RoutinesFromPaper

--- src/main/java/com/treasure/hunt/strategy/searcher/impl/strategyFromPaper/RoutinesFromPaper.java
+++ src/main/java/com/treasure/hunt/strategy/searcher/impl/strategyFromPaper/RoutinesFromPaper.java
@@ -19,6 +19,8 @@ import static com.treasure.hunt.utils.JTSUtils.lineWayIntersection;
  * For an explanation what each method does, it is recommended to look in the paper (Deterministic Treasure Hunt in the
  * Plane with Angular Hints from Bouchard et al.).
  * There the same symbols are used.
+ * The functionality of rectangleScan is explained there on page 3.
+ * Phi, rho, sigma and the basic-transformation are explained on pages 7ff.
  *
  * @author Rank
  */
@@ -27,26 +29,8 @@ public class RoutinesFromPaper {
     }
 
     /**
-     * Does the same as the routine rectangleScan in the paper.
-     * It adds the Points to move so that the player sees all points in the rectangle ABCD.
-     *
-     * @param A
-     * @param B
-     * @param C
-     * @param D
-     * @param move
-     * @return
-     */
-    public static SearchPath rectangleScan(Point A, Point B, Point C, Point D, SearchPath move) {
-        return rectangleScan(A.getCoordinate(), B.getCoordinate(), C.getCoordinate(), D.getCoordinate(), move);
-    }
-
-    /**
-     * Returns a Array of Points which are on the line P1P2 and which have distance one to one another.
-     *
-     * @param P1
-     * @param P2
-     * @return
+     * @param k the distance from P1 to P2
+     * @return An Array of Points on P1P2, which have distance one to one another and reach from P1 to P2 at minimum
      */
     static private Point[] lineOfPointsWithDistanceOne(int k, Coordinate P1, Coordinate P2) {
         Point[] res = new Point[k + 1];
@@ -61,17 +45,18 @@ public class RoutinesFromPaper {
     }
 
     /**
-     * Does the same as the routine rectangleScan in the paper.
-     * It adds the Points to move so that the player sees all points in the rectangle ABCD.
-     *
-     * @param A
-     * @param B
-     * @param C
-     * @param D
-     * @param move
-     * @return
+     * Does the same as the routine RectangleScan in the paper.
+     * It adds the Points to searchPath so that the player sees all points in the rectangle ABCD.
+     * Unlike the paper it does not add the point where the procedure started to the search-path.
      */
-    public static SearchPath rectangleScan(Coordinate A, Coordinate B, Coordinate C, Coordinate D, SearchPath move) {
+    public static SearchPath rectangleScan(Point A, Point B, Point C, Point D, SearchPath searchPath) {
+        return rectangleScan(A.getCoordinate(), B.getCoordinate(), C.getCoordinate(), D.getCoordinate(), searchPath);
+    }
+
+    /**
+     * @see RoutinesFromPaper#rectangleScan(Point, Point, Point, Point, SearchPath)
+     */
+    public static SearchPath rectangleScan(Coordinate A, Coordinate B, Coordinate C, Coordinate D, SearchPath searchPath) {
         if (A.distance(B) > A.distance(D)) {
             Coordinate temp = A;
             A = D;
@@ -83,9 +68,9 @@ public class RoutinesFromPaper {
         int k = (int) A.distance(B);
 
         if (k == 0) {
-            move.addPoint(JTSUtils.GEOMETRY_FACTORY.createPoint(A));
-            move.addPoint(JTSUtils.GEOMETRY_FACTORY.createPoint(C));
-            return move;
+            searchPath.addPoint(JTSUtils.GEOMETRY_FACTORY.createPoint(A));
+            searchPath.addPoint(JTSUtils.GEOMETRY_FACTORY.createPoint(C));
+            return searchPath;
         }
 
         Point[] a = lineOfPointsWithDistanceOne(k, A, B);
@@ -93,35 +78,35 @@ public class RoutinesFromPaper {
 
         if (k % 2 == 1) {
             for (int i = 0; i <= k - 1; i += 2) {
-                move.addPoint(a[i]);
-                move.addPoint(b[i]);
-                move.addPoint(b[i + 1]);
-                move.addPoint(a[i + 1]);
+                searchPath.addPoint(a[i]);
+                searchPath.addPoint(b[i]);
+                searchPath.addPoint(b[i + 1]);
+                searchPath.addPoint(a[i + 1]);
             }
         } else {
             for (int i = 0; i <= k - 2; i += 2) {
-                move.addPoint(a[i]);
-                move.addPoint(b[i]);
-                move.addPoint(b[i + 1]);
-                move.addPoint(a[i + 1]);
+                searchPath.addPoint(a[i]);
+                searchPath.addPoint(b[i]);
+                searchPath.addPoint(b[i + 1]);
+                searchPath.addPoint(a[i + 1]);
             }
-            move.addPoint(a[k]);
-            move.addPoint(b[k]);
+            searchPath.addPoint(a[k]);
+            searchPath.addPoint(b[k]);
         }
-        return move;
+        return searchPath;
     }
 
     /**
-     * Returns the result of rho, defined by rectangle rect, applied on hint.
+     * Returns the result of rho, defined rectangle, applied on hint.
      *
-     * @param rect the rectangle that defines rho
+     * @param rectangle the rectangle defining rho
      * @param hint used as input in rho
      * @return the result of rho(hint)
      */
-    static HalfPlaneHint rhoHint(Coordinate[] rect, HalfPlaneHint hint) {
-        assertRectangle(rect);
-        LineSegment AB = new LineSegment(rect[0], rect[1]);
-        LineSegment CD = new LineSegment(rect[2], rect[3]);
+    static HalfPlaneHint rhoHint(Coordinate[] rectangle, HalfPlaneHint hint) {
+        assertRectangle(rectangle);
+        LineSegment AB = new LineSegment(rectangle[0], rectangle[1]);
+        LineSegment CD = new LineSegment(rectangle[2], rectangle[3]);
         Coordinate centerAB = AB.midPoint();
         Coordinate centerCD = CD.midPoint();
         AffineTransformation reflection = AffineTransformation.reflectionInstance(centerAB.getX(), centerAB.getY(),
@@ -135,16 +120,16 @@ public class RoutinesFromPaper {
     }
 
     /**
-     * Returns the result of rho, defined by rectangle rect, applied on P.
+     * Returns the result of rho, defined by rectangle, applied on P.
      *
-     * @param rect the rectangle that defines rho
+     * @param rectangle the rectangle defining rho
      * @param P    used as input in rho
      * @return the result of rho(P)
      */
-    static Coordinate rhoPoint(Coordinate[] rect, Coordinate P) {
-        assertRectangle(rect);
-        LineSegment AB = new LineSegment(rect[0], rect[1]);
-        LineSegment CD = new LineSegment(rect[2], rect[3]);
+    static Coordinate rhoPoint(Coordinate[] rectangle, Coordinate P) {
+        assertRectangle(rectangle);
+        LineSegment AB = new LineSegment(rectangle[0], rectangle[1]);
+        LineSegment CD = new LineSegment(rectangle[2], rectangle[3]);
         Coordinate centerAB = AB.midPoint();
         Coordinate centerCD = CD.midPoint();
         AffineTransformation reflection = AffineTransformation.reflectionInstance(centerAB.getX(), centerAB.getY(),
@@ -160,8 +145,8 @@ public class RoutinesFromPaper {
      *
      * @param i the index of sigma
      * @param r the center of a rectangle which is used as the point to rotate around
-     * @param P the Point which is to be transformed
-     * @return
+     * @param P the Point which is used as input in sigma
+     * @return sigma_i(P), with sigma being defined by r
      */
     static Coordinate sigmaPoint(int i, Coordinate r, Coordinate P) {
         AffineTransformation rotationI; //rot_i
@@ -189,6 +174,9 @@ public class RoutinesFromPaper {
 
     }
 
+    /**
+     * Reverses {@link RoutinesFromPaper#sigmaPoint(int, Coordinate, Coordinate)}
+     */
     static Coordinate sigmaPointReverse(int i, Coordinate r, Coordinate P) {
         if (i < 0 || i > 3) {
             throw new IllegalArgumentException("i should be in [0,3] but is equal to " + i);
@@ -197,28 +185,28 @@ public class RoutinesFromPaper {
     }
 
     /**
-     * Returns the result of sigma, defined by index i and rectangle rect, applied on the points in rect.
+     * Returns the result of sigma_i defined by rectangle, applied on the points in rectangle.
      *
-     * @param i
-     * @param rect
-     * @return
+     * @param i    the index of sigma
+     * @param rectangle the rectangle sigma is defined by and applied on
+     * @return sigma_i(rectangle) with sigma being defined by rectangle
      */
-    static Coordinate[] sigmaRectangle(int i, Coordinate[] rect) {
-        assertRectangle(rect);
+    static Coordinate[] sigmaRectangle(int i, Coordinate[] rectangle) {
+        assertRectangle(rectangle);
 
         if (i == 0 || i == 2) {
-            return rect;
+            return rectangle;
         }
 
-        Coordinate r = centerOfRectangle(rect);
+        Coordinate r = centerOfRectangle(rectangle);
         AffineTransformation rotHalfPi = new AffineTransformation(new double[]{0, -1, 0, 1, 0, 0});
         if (i == 1 || i == 3) {
             //rotate rectangle by pi/2
             Coordinate[] transformed = new Coordinate[]{new Coordinate(), new Coordinate(),
                     new Coordinate(), new Coordinate()};
             for (int j = 0; j <= 3; j++) {
-                transformed[j].x = rect[j].x - r.x;
-                transformed[j].y = rect[j].y - r.y;
+                transformed[j].x = rectangle[j].x - r.x;
+                transformed[j].y = rectangle[j].y - r.y;
             }
 
             Coordinate transformed0old = transformed[0].copy();
@@ -237,47 +225,52 @@ public class RoutinesFromPaper {
     }
 
     /**
-     * Returns the result of phi, defined by rect, with index i, applied on rect.
+     * Returns the result of phi, defined by rectangle, with index i, applied on rectangle.
      *
-     * @param i
-     * @param rect
-     * @return
+     * @param i    the index of phi
+     * @param rectangle the rectangle phi is defined by and applied on
+     * @return phi_i(rectangle), with phi being defined by rectangle
      */
-    static Coordinate[] phiRectangle(int i, Coordinate[] rect) {
-        assertRectangle(rect);
+    static Coordinate[] phiRectangle(int i, Coordinate[] rectangle) {
+        assertRectangle(rectangle);
         if (i < 0 || i > 7) {
             throw new IllegalArgumentException("i must be in [0,7] but is " + i);
         }
-        return sigmaRectangle(i % 4, rect);
-    }
-
-
-    static Coordinate phiPoint(int i, Coordinate[] rect, Coordinate P) {
-        assertRectangle(rect);
-        if (i < 0 || i > 7) {
-            throw new IllegalArgumentException("i must be in [0,7] but is " + i);
-        }
-        Coordinate r = centerOfRectangle(rect);
-        if (i < 4) {
-            return sigmaPoint(i, r, P);
-        }
-        return rhoPoint(rect, sigmaPoint(i, r, P));
+        return sigmaRectangle(i % 4, rectangle);
     }
 
     /**
-     * Returns the result of phi, defined by rect, with index i, applied on hint.
-     *
      * @param i    the index of phi
-     * @param rect the rectangle which defines phi
-     * @param hint the hint that is used as input in phi
-     * @return
+     * @param rectangle the rectangle defining phi
+     * @param P    the point phi is applied on
+     * @return phi_i(P), with phi being defined by rectangle
      */
-    static HalfPlaneHint phiHint(int i, Coordinate[] rect, HalfPlaneHint hint) {
-        assertRectangle(rect);
+    static Coordinate phiPoint(int i, Coordinate[] rectangle, Coordinate P) {
+        assertRectangle(rectangle);
         if (i < 0 || i > 7) {
             throw new IllegalArgumentException("i must be in [0,7] but is " + i);
         }
-        Coordinate r = centerOfRectangle(rect);
+        Coordinate r = centerOfRectangle(rectangle);
+        if (i < 4) {
+            return sigmaPoint(i, r, P);
+        }
+        return rhoPoint(rectangle, sigmaPoint(i, r, P));
+    }
+
+    /**
+     * Returns the result of phi, defined by rectangle, with index i, applied on hint.
+     *
+     * @param i    the index of phi
+     * @param rectangle the rectangle defining phi
+     * @param hint the hint that is used as input in phi
+     * @return phi_i(hint), with phi being defined by rectangle
+     */
+    static HalfPlaneHint phiHint(int i, Coordinate[] rectangle, HalfPlaneHint hint) {
+        assertRectangle(rectangle);
+        if (i < 0 || i > 7) {
+            throw new IllegalArgumentException("i must be in [0,7] but is " + i);
+        }
+        Coordinate r = centerOfRectangle(rectangle);
         HalfPlaneHint transformedHint = new HalfPlaneHint(
                 sigmaPoint(i % 4, r, hint.getCenter()),
                 sigmaPoint(i % 4, r, hint.getRight())
@@ -285,52 +278,45 @@ public class RoutinesFromPaper {
         if (i < 4) {
             return transformedHint;
         }
-        return rhoHint(rect, transformedHint);
+        return rhoHint(rectangle, transformedHint);
     }
 
     /**
-     * Calculates the inverse phi operation defined by the rectangle rect and applies it on P.
+     * Calculates the inverse of the phi operation defined by rectangle and applies it on P.
      *
      * @param i    the index of phi
-     * @param rect the rectangle which defines phi (and therefore also the inverse of phi)
-     * @param P
-     * @return
+     * @param rectangle the rectangle defining phi (and therefore also the inverse of phi)
+     * @param P    the point the inverse of phi gets as input
+     * @return the result of the inverse of phi_i applied on P, with phi being defined by rectangle
      */
-    static Coordinate phiPointInverse(int i, Coordinate[] rect, Coordinate P) {
-        assertRectangle(rect);
+    static Coordinate phiPointInverse(int i, Coordinate[] rectangle, Coordinate P) {
+        assertRectangle(rectangle);
         if (i < 0 || i > 7) {
             throw new IllegalArgumentException("i must be in [0,7] but is " + i);
         }
 
-        Coordinate r = centerOfRectangle(rect);
+        Coordinate r = centerOfRectangle(rectangle);
         if (i < 4) {
             return sigmaPointReverse(i, r, P);
         }
-        return sigmaPointReverse(i - 4, r, rhoPoint(rect, P));
+        return sigmaPointReverse(i - 4, r, rhoPoint(rectangle, P));
     }
 
     /**
-     * Calculate phi defined by basicTrans and phiRect of the rectangle ABCD.
-     * Add the steps to scan this rectangle with rectangleScan to move.
+     * Calculate phi_basicTrans, with phi being defined by the rectangle phiRect
+     * Then adds the steps of the rectangleScan of phi_basicTrans(ABCD) to the searchPath.
      *
-     * @param basicTrans
-     * @param phiRect
-     * @param A
-     * @param B
-     * @param C
-     * @param D
-     * @param strategy the strategy whose specificRectangleScan should be used
-     * @param move
-     * @return
+     * @param strategy   the strategy whose specificRectangleScan should be used
+     * @return the input searchPath, rectangleScan applied to it
      */
     static SearchPath rectangleScanPhiReverse(int basicTrans, Coordinate[] phiRect, Coordinate A, Coordinate B,
-                                              Coordinate C, Coordinate D, SearchPath move, StrategyFromPaper strategy) {
+                                              Coordinate C, Coordinate D, SearchPath searchPath, StrategyFromPaper strategy) {
         return strategy.specificRectangleScan(
                 phiPointInverse(basicTrans, phiRect, A),
                 phiPointInverse(basicTrans, phiRect, B),
                 phiPointInverse(basicTrans, phiRect, C),
                 phiPointInverse(basicTrans, phiRect, D),
-                move
+                searchPath
         );
     }
 
@@ -338,14 +324,12 @@ public class RoutinesFromPaper {
      * Returns the basic transformation for a rectangle-hint-pair, of which the definition can be found in the paper.
      * (Page 8, below Proposition 3.2)
      *
-     * @param rect
-     * @param hint
-     * @return
+     * @return the basic transformation of this rectangle-hint pair
      */
-    static int getBasicTransformation(Coordinate[] rect, HalfPlaneHint hint) {
+    static int getBasicTransformation(Coordinate[] rectangle, HalfPlaneHint hint) {
         for (int i = 0; i <= 7; i++) {
-            Coordinate[] testRect = phiRectangle(i, rect);
-            HalfPlaneHint testHint = phiHint(i, rect, hint);
+            Coordinate[] testRect = phiRectangle(i, rectangle);
+            HalfPlaneHint testHint = phiHint(i, rectangle, hint);
             LineSegment hintLine = new LineSegment(testHint.getCenter(),
                     testHint.getRight());
             LineSegment testAD = new LineSegment(testRect[0], testRect[3]);
@@ -360,28 +344,27 @@ public class RoutinesFromPaper {
             }
         }
         throw new IllegalArgumentException("Somehow there was no basic transformation found for this " +
-                "rectangle and hint. This is impossible.\n" + "rect: " + Arrays.toString(rect) + " anglepoints: "
-                + hint.getRight() + hint.getCenter());
+                "rectangle and hint. This is impossible.\n" + "rectangle: " + Arrays.toString(rectangle) +
+                " anglepoints: " + hint.getRight() + hint.getCenter());
     }
 
     /**
-     * Inverts the by rect defined phi , but calculates and returns the inverse points of the points in toTransform.
+     * Inverts the by rectangle defined phi_i, and calculates and returns the inverse points of the points in toTransform.
      *
      * @param i           the index of phi
-     * @param rect        the rectangle which defines phi (and therefore also the inverse of phi)
-     * @param toTransform
-     * @return
+     * @param rectangle        the rectangle defining phi (and therefore also the inverse of phi)
+     * @param toTransform the input for the inverse of phi
+     * @return the inverse of phi_i defined by rectangle, applied on toTransform
      */
-    static Coordinate[] phiOtherRectangleInverse(int i, Coordinate[] rect, Coordinate[] toTransform) {
-        assertRectangle(rect);
+    static Coordinate[] phiOtherRectangleInverse(int i, Coordinate[] rectangle, Coordinate[] toTransform) {
+        assertRectangle(rectangle);
         assertRectangle(toTransform);
         Coordinate[] ret = new Coordinate[]{
-                phiPointInverse(i, rect, toTransform[0]),
-                phiPointInverse(i, rect, toTransform[1]),
-                phiPointInverse(i, rect, toTransform[2]),
-                phiPointInverse(i, rect, toTransform[3])
+                phiPointInverse(i, rectangle, toTransform[0]),
+                phiPointInverse(i, rectangle, toTransform[1]),
+                phiPointInverse(i, rectangle, toTransform[2]),
+                phiPointInverse(i, rectangle, toTransform[3])
         };
         return arrangeRectangle(ret);
     }
-
 }
