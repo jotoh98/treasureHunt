30609c491bd1d94c0a56b0205bd7329fbd1944af
dorianreineccius <email@example.com>
2020-03-30 22:07:55 +0200

    import failed

--- src/main/java/com/treasure/hunt/strategy/searcher/impl/PolygonStrategy.java
+++ src/main/java/com/treasure/hunt/strategy/searcher/impl/PolygonStrategy.java
@@ -1,11 +1,11 @@
 package com.treasure.hunt.strategy.searcher.impl;
 
-import com.treasure.hunt.game.mods.hideandseek.HideAndSeekSearcher;
 import com.treasure.hunt.jts.geom.GeometryAngle;
 import com.treasure.hunt.strategy.geom.GeometryItem;
 import com.treasure.hunt.strategy.geom.GeometryType;
 import com.treasure.hunt.strategy.hint.impl.AngleHint;
 import com.treasure.hunt.strategy.searcher.SearchPath;
+import com.treasure.hunt.strategy.searcher.Searcher;
 import com.treasure.hunt.utils.JTSUtils;
 import lombok.extern.slf4j.Slf4j;
 import org.locationtech.jts.geom.*;
