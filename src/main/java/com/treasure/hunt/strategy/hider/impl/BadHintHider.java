70c91188c9fa375a69c78daa942a4ecd70e2c7ad
dorianreineccius <email@example.com>
2020-04-02 15:33:57 +0200

    Command + alt + l

--- src/main/java/com/treasure/hunt/strategy/hider/impl/BadHintHider.java
+++ src/main/java/com/treasure/hunt/strategy/hider/impl/BadHintHider.java
@@ -12,7 +12,6 @@ import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.GeometryFactory;
 import org.locationtech.jts.geom.Point;
 import org.locationtech.jts.geom.Polygon;
-import org.locationtech.jts.util.GeometricShapeFactory;
 
 import java.util.List;
 import java.util.NoSuchElementException;
@@ -22,6 +21,7 @@ public class BadHintHider implements Hider<HalfPlaneHint> {
 
     private Point treasure;
     private GeometryFactory GEOMETRY_FACTORY = JTSUtils.GEOMETRY_FACTORY;
+
     @Override
     public void init(final Point searcherStartPosition) {
         treasure = JTSUtils.shuffleTreasure();
@@ -37,56 +37,53 @@ public class BadHintHider implements Hider<HalfPlaneHint> {
 
         try {
 
-            GeometryItem<Polygon>  currentRectangleGeometryItem=  (GeometryItem<Polygon>) geometryItems.stream().filter( item -> item.getGeometryType().getDisplayName().equals("current rectangle")).findFirst().get();
+            GeometryItem<Polygon> currentRectangleGeometryItem = (GeometryItem<Polygon>) geometryItems.stream().filter(item -> item.getGeometryType().getDisplayName().equals("current rectangle")).findFirst().get();
             currentRectangle = currentRectangleGeometryItem.getObject();
 
-        }catch (NoSuchElementException e){
+        } catch (NoSuchElementException e) {
             EventBusUtils.LOG_LABEL_EVENT.trigger(getClass().getSimpleName() + " didn't find current rectangle. Are you playing against " + StrategyFromPaper.class.getSimpleName() + "?");
             log.trace("generic rectangle");
             // else give a 45degree / 135degree Hint relative to x-axis
             Coordinate[] rectangleCoords = new Coordinate[5];
             rectangleCoords[0] = new Coordinate(-10000, 10000);
-            rectangleCoords[1] = new Coordinate( 10000, 10000);
-            rectangleCoords[2] = new Coordinate( 10000, -10000);
+            rectangleCoords[1] = new Coordinate(10000, 10000);
+            rectangleCoords[2] = new Coordinate(10000, -10000);
             rectangleCoords[3] = new Coordinate(-10000, -10000);
             rectangleCoords[4] = new Coordinate(-10000, 10000);
             currentRectangle = GEOMETRY_FACTORY.createPolygon(rectangleCoords);
 
         }
-            // quick sanity check
-            log.trace("is rectangle?" +  currentRectangle.isRectangle());
+        // quick sanity check
+        log.trace("is rectangle?" + currentRectangle.isRectangle());
 
-            Coordinate topLeft = currentRectangle.getCoordinates()[0].copy();
-            topLeft.y = topLeft.y -0.1;
-            Coordinate bottomRight = currentRectangle.getCoordinates()[2].copy();
-            bottomRight.y = bottomRight.y + 0.1;
+        Coordinate topLeft = currentRectangle.getCoordinates()[0].copy();
+        topLeft.y = topLeft.y - 0.1;
+        Coordinate bottomRight = currentRectangle.getCoordinates()[2].copy();
+        bottomRight.y = bottomRight.y + 0.1;
 
-            for(Coordinate c : currentRectangle.getCoordinates()){
-                log.trace(c.toString());
-            }
+        for (Coordinate c : currentRectangle.getCoordinates()) {
+            log.trace(c.toString());
+        }
 
+        // there's 2 possible Hint now, only one will contain the treasure
 
-            // there's 2 possible Hint now, only one will contain the treasure
+        HalfPlaneHint badHint = new HalfPlaneHint(playerPosition.getCoordinate(), bottomRight); // upward right facing
 
-
-            HalfPlaneHint badHint = new HalfPlaneHint(playerPosition.getCoordinate(), bottomRight); // upward right facing
-
-            log.trace("the up right facing Hint contains treasure: " + badHint.getGeometryAngle().inView(treasure.getCoordinate()));
+        log.trace("the up right facing Hint contains treasure: " + badHint.getGeometryAngle().inView(treasure.getCoordinate()));
         if (!badHint.getGeometryAngle().inView(treasure.getCoordinate())) {
 
-            badHint = new HalfPlaneHint(playerPosition.getCoordinate(),bottomRight); // bottom left facing
+            badHint = new HalfPlaneHint(playerPosition.getCoordinate(), bottomRight); // bottom left facing
             log.trace("the bottom left facing Hint contains treasure: " + badHint.getGeometryAngle().inView(treasure.getCoordinate()));
-
 
             // strategyFromPaper is currently performing a scan or the 2-distance step away from the centroid
             if (!badHint.getGeometryAngle().inView(treasure.getCoordinate())) {
 
                 log.info("can't give diagonal Hint, returning generic 45degree one");
 
-                badHint = new HalfPlaneHint(playerPosition.getCoordinate(), new Coordinate( playerPosition.getX()+1, playerPosition.getY()-1)); //up right first again
+                badHint = new HalfPlaneHint(playerPosition.getCoordinate(), new Coordinate(playerPosition.getX() + 1, playerPosition.getY() - 1)); //up right first again
 
-                if (!badHint.getGeometryAngle().inView(treasure.getCoordinate())){
-                    badHint = new HalfPlaneHint(playerPosition.getCoordinate(), new Coordinate( playerPosition.getX()-1, playerPosition.getY() + 1)); //else bottom left
+                if (!badHint.getGeometryAngle().inView(treasure.getCoordinate())) {
+                    badHint = new HalfPlaneHint(playerPosition.getCoordinate(), new Coordinate(playerPosition.getX() - 1, playerPosition.getY() + 1)); //else bottom left
                 }
             }
 
