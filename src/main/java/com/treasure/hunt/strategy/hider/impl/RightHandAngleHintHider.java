6c3d11e7aea49b3359573abf194e96fa71d15fc0
kemna <ruben-kemna@web.de>
2020-04-03 19:46:18 +0200

    changed Angle preference from Radians to Degree Merged Preference "Angle" and "preferred angle size" if angle == 180, explicit HalfPlane is returned

--- src/main/java/com/treasure/hunt/strategy/hider/impl/RightHandAngleHintHider.java
+++ src/main/java/com/treasure/hunt/strategy/hider/impl/RightHandAngleHintHider.java
@@ -4,8 +4,10 @@ import com.treasure.hunt.service.preferences.Preference;
 import com.treasure.hunt.service.preferences.PreferenceService;
 import com.treasure.hunt.strategy.hider.Hider;
 import com.treasure.hunt.strategy.hint.impl.AngleHint;
+import com.treasure.hunt.strategy.hint.impl.HalfPlaneHint;
 import com.treasure.hunt.strategy.searcher.SearchPath;
 import com.treasure.hunt.utils.JTSUtils;
+import org.locationtech.jts.algorithm.Angle;
 import org.locationtech.jts.geom.Coordinate;
 import org.locationtech.jts.geom.Point;
 import org.locationtech.jts.math.Vector2D;
@@ -18,7 +20,7 @@ import org.locationtech.jts.math.Vector2D;
  * @author dorianreineccius
  */
 @Preference(name = PreferenceService.MAX_TREASURE_DISTANCE, value = 100)
-@Preference(name = PreferenceService.ANGLE, value = Math.PI)
+@Preference(name = PreferenceService.HintSize_Preference, value = 180)
 public class RightHandAngleHintHider implements Hider<AngleHint> {
     private Point treasurePosition;
 
@@ -47,10 +49,14 @@ public class RightHandAngleHintHider implements Hider<AngleHint> {
     @Override
     public AngleHint move(SearchPath searchPath) {
         Coordinate searcherPos = searchPath.getLastPoint().getCoordinate();
-        double angle = PreferenceService.getInstance().getPreference(PreferenceService.ANGLE, Math.PI * 2).doubleValue();
+        double angle = Angle.toRadians(PreferenceService.getInstance().getPreference(PreferenceService.HintSize_Preference, 180).doubleValue());
 
+        if (PreferenceService.getInstance().getPreference(PreferenceService.HintSize_Preference, 180).intValue() == 180){
+            return new HalfPlaneHint(searcherPos, treasurePosition.getCoordinate());
+        }
         Vector2D vector2D = new Vector2D(searcherPos, treasurePosition.getCoordinate());
         vector2D = vector2D.rotate(angle);
+
         return new AngleHint(
                 treasurePosition.getCoordinate(),
                 searcherPos,
